dd3bdf7661071402a67a76c272eda859a5cdfda5
waleedmustafa2002 <waleedmustafa004@gmail.com>
2026-05-05 20:29:38 +0500

    Add increment and decrement number using while loop

---
+++ 05-05-2026/Reversenumber-code.js
@@ -0,0 +1,16 @@
+
+  let r=1;
+  console.log("Increment Number");
+  while(r<=3){
+       console.log(r)
+     r++;
+  }
+
+
+  let i = 3;
+  console.log("Decrement Number");
+  while (i >= 1) {
+      console.log(i)
+      i--;
+  }
+  
