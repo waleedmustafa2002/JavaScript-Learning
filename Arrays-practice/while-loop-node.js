241bafa37799db0523af9cfede17c97e8e33a862
waleedmustafa2002 <waleedmustafa004@gmail.com>
2026-05-09 17:13:01 +0500

    Add array practice tasks using while loops

---
+++ Arrays-practice/while-loop-node.js
@@ -0,0 +1,70 @@
+//Array task while loop used:
+//Print array using while loop
+
+// let nums = [5, 10, 15, 20, 25];
+
+// let i = 0;
+
+// while (i < nums.length) {
+//     console.log(nums[i]);
+//     i++;
+// }
+
+
+
+// //Sum of array using while loop
+
+// let nums = [1, 2, 3, 4, 5];
+
+// let i = 0;
+// let sum = 0;
+
+// while (i < nums.length) {
+//     sum = sum + nums[i];
+//     i++;
+// }
+
+// console.log(sum);
+
+
+// //Print even numbers from array
+
+// let nums= [2, 5, 8, 11, 14];
+// let i=0;
+// while(i<nums.length){
+//     if(nums[i]%2===0){
+//         console.log(nums[i]);
+//     }
+// i++;
+// }
+
+
+
+// //Find largest number
+
+// let nums=[10, 50, 30, 80, 20]
+// let i=0;
+// let max=nums[0];
+// while(i<nums.length) {
+//     if(nums[i]> max){
+//         max=nums[i]
+//     }
+//     i++;
+// }
+// console.log(max);
+
+
+
+// //Count numbers greater than 10
+
+// let nums=[5, 12, 8, 20, 15];
+// let i=0;
+// let count=0;
+// while(i<nums.length){
+//     if(nums[i]>10){
+//         count++;
+//     }
+//     i++;
+
+// }
+// console.log(count);
