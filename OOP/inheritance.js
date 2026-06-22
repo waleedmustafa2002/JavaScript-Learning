4b88e082a98e8b4f3c9509ef349844867baef28c
waleedmustafa2002 <waleedmustafa004@gmail.com>
2026-06-22 13:15:58 +0500

    add hierarchical-inheritance.js

--- OOP/inheritance.js
+++ OOP/inheritance.js
@@ -1,3 +1,160 @@
+// class Person {
+//     speak() {
+//         console.log("Person is speaking");
+//     }
+// }
+
+// class Student extends Person {
+//     study() {
+//         console.log("Student is studying");
+//     }
+// }
+
+// let s1 = new Student();
+
+// s1.speak();
+// s1.study();
+
+
+
+
+
+
+// class Animal {
+//     eat() {
+//         console.log("Animal is eating");
+//     }
+// }
+
+// class Cat extends Animal {
+//     meow() {
+//         console.log("Cat says Meow");
+//     }
+// }
+
+// let c1 = new Cat();
+
+// c1.eat();
+// c1.meow();
+
+
+
+
+
+// class Vehicle {
+//     start() {
+//         console.log("Vehicle started");
+//     }
+// }
+
+// class Bike extends Vehicle {
+//     ride() {
+//         console.log("Bike is riding");
+//     }
+// }
+
+// let b1 = new Bike();
+
+// b1.start();
+// b1.ride();
+
+
+
+
+// //Constructor Inheritance
+
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// class Student extends Person {
+// }
+
+// let s1 = new Student("Ali");
+
+// console.log(s1.name);
+
+
+
+
+
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// class Student extends Person {
+//     constructor(name, grade) {
+//         super(name);
+//         this.grade = grade;
+//     }
+// }
+
+// let s1 = new Student("Ali", "A");
+
+// console.log(s1.name);
+// console.log(s1.grade);
+
+
+
+// //Multilevel inheritance
+
+// class Person {
+//     speak() {
+//         console.log("Person is speaking");
+//     }
+// }
+
+// class Student extends Person {
+//     study() {
+//         console.log("Student is studying");
+//     }
+// }
+
+// class Monitor extends Student {
+//     manageClass() {
+//         console.log("Monitor is managing class");
+//     }
+// }
+
+// let m1 = new Monitor();
+
+// m1.speak();
+// m1.study();
+// m1.manageClass();
+
+
+
+// class Vehicle {
+//     start() {
+//         console.log("Vehicle started");
+//     }
+// }
+
+// class Car extends Vehicle {
+//     drive() {
+//         console.log("Car is driving");
+//     }
+// }
+
+// class SportsCar extends Car {
+//     turbo() {
+//         console.log("Turbo mode ON");
+//     }
+// }
+
+// let s1 = new SportsCar();
+
+// s1.start();
+// s1.drive();
+// s1.turbo();
+
+
+
+//Hierarchical Inheritance
 class Person {
     speak() {
         console.log("Person is speaking");
@@ -10,90 +167,47 @@ class Student extends Person {
     }
 }
 
+class Teacher extends Person {
+    teach() {
+        console.log("Teacher is teaching");
+    }
+}
+
 let s1 = new Student();
+let t1 = new Teacher();
 
 s1.speak();
 s1.study();
 
+t1.speak();
+t1.teach();
 
 
 
 
-
-class Animal {
-    eat() {
-        console.log("Animal is eating");
+class Employee {
+    work() {
+        console.log("Employee is working");
     }
 }
 
-class Cat extends Animal {
-    meow() {
-        console.log("Cat says Meow");
+class Manager extends Employee {
+    manage() {
+        console.log("Manager is managing");
     }
 }
 
-let c1 = new Cat();
-
-c1.eat();
-c1.meow();
-
-
-
-
-
-class Vehicle {
-    start() {
-        console.log("Vehicle started");
+class Developer extends Employee {
+    code() {
+        console.log("Developer is coding");
     }
 }
 
-class Bike extends Vehicle {
-    ride() {
-        console.log("Bike is riding");
-    }
-}
+let m1 = new Manager();
+let d1 = new Developer();
 
-let b1 = new Bike();
+m1.work();
+m1.manage();
 
-b1.start();
-b1.ride();
-
-
-
-
-//Constructor Inheritance
-
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-}
-
-class Student extends Person {
-}
-
-let s1 = new Student("Ali");
-
-console.log(s1.name);
-
-
-
-
-
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-}
-
-class Student extends Person {
-    constructor(name, grade) {
-        super(name);
-        this.grade = grade;
-    }
-}
-
-let s1 = new Student("Ali", "A");
-
-console.log(s1.name);
-console.log(s1.grade);
+d1.work();
+d1.code();
