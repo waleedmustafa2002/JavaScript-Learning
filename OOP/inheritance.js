3555e5fcfb748d105751eacc536dc01359d618cb
waleedmustafa2002 <waleedmustafa004@gmail.com>
2026-06-22 12:53:07 +0500

    add multilevel-inheritance.js

--- OOP/inheritance.js
+++ OOP/inheritance.js
@@ -1,3 +1,107 @@
+// class Person {
+//     speak() {
+//         console.log("Person is speaking");
+//     }
+// }
+
+// class Student extends Person {
+//     study() {
+//         console.log("Student is studying");
+//     }
+// }
+
+// let s1 = new Student();
+
+// s1.speak();
+// s1.study();
+
+
+
+
+
+
+// class Animal {
+//     eat() {
+//         console.log("Animal is eating");
+//     }
+// }
+
+// class Cat extends Animal {
+//     meow() {
+//         console.log("Cat says Meow");
+//     }
+// }
+
+// let c1 = new Cat();
+
+// c1.eat();
+// c1.meow();
+
+
+
+
+
+// class Vehicle {
+//     start() {
+//         console.log("Vehicle started");
+//     }
+// }
+
+// class Bike extends Vehicle {
+//     ride() {
+//         console.log("Bike is riding");
+//     }
+// }
+
+// let b1 = new Bike();
+
+// b1.start();
+// b1.ride();
+
+
+
+
+// //Constructor Inheritance
+
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// class Student extends Person {
+// }
+
+// let s1 = new Student("Ali");
+
+// console.log(s1.name);
+
+
+
+
+
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// class Student extends Person {
+//     constructor(name, grade) {
+//         super(name);
+//         this.grade = grade;
+//     }
+// }
+
+// let s1 = new Student("Ali", "A");
+
+// console.log(s1.name);
+// console.log(s1.grade);
+
+
+
+//Multilevel inheritance
+
 class Person {
     speak() {
         console.log("Person is speaking");
@@ -10,34 +114,17 @@ class Student extends Person {
     }
 }
 
-let s1 = new Student();
-
-s1.speak();
-s1.study();
-
-
-
-
-
-
-class Animal {
-    eat() {
-        console.log("Animal is eating");
+class Monitor extends Student {
+    manageClass() {
+        console.log("Monitor is managing class");
     }
 }
 
-class Cat extends Animal {
-    meow() {
-        console.log("Cat says Meow");
-    }
-}
+let m1 = new Monitor();
 
-let c1 = new Cat();
-
-c1.eat();
-c1.meow();
-
-
+m1.speak();
+m1.study();
+m1.manageClass();
 
 
 
@@ -47,53 +134,20 @@ class Vehicle {
     }
 }
 
-class Bike extends Vehicle {
-    ride() {
-        console.log("Bike is riding");
+class Car extends Vehicle {
+    drive() {
+        console.log("Car is driving");
     }
 }
 
-let b1 = new Bike();
-
-b1.start();
-b1.ride();
-
-
-
-
-//Constructor Inheritance
-
-class Person {
-    constructor(name) {
-        this.name = name;
+class SportsCar extends Car {
+    turbo() {
+        console.log("Turbo mode ON");
     }
 }
 
-class Student extends Person {
-}
+let s1 = new SportsCar();
 
-let s1 = new Student("Ali");
-
-console.log(s1.name);
-
-
-
-
-
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-}
-
-class Student extends Person {
-    constructor(name, grade) {
-        super(name);
-        this.grade = grade;
-    }
-}
-
-let s1 = new Student("Ali", "A");
-
-console.log(s1.name);
-console.log(s1.grade);
+s1.start();
+s1.drive();
+s1.turbo();
